55f3387765c521f552e244f64fcd9c024d9ff1a0
Aleksey Vilchinskiy <avilchinsky@gmail.com>
2020-09-28 22:18:03 +0700

    product card

--- product_page/main.js
+++ product_page/main.js
@@ -1,6 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: './assets/product_red.jpg',
+        inStock: true,
+        details: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral"
+        ],
+        variants: [
+            {
+                id: 123,
+                color: "red"
+            },
+            {
+                id: 1234,
+                color: "blue"
+            }
+        ]
     }
 })
